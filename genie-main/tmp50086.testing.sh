87e15797155453376c15162a4401d75242fe7175
R.G.Stockey <rgs1e22@cyan52.cluster.local>
2023-10-03 14:29:46 +0100

    muffins update for cluster use

--- genie-main/tmp50086.testing.sh
+++
@@ -1,5 +0,0 @@
-TEST_NAME=muffin.CB.V21a_097.BASES-2.config_regtest
-ASSUMEDGOOD_NAME=muffin.CB.V21a_097.BASES-2.config_assumedgood
-KNOWNGOOD_NAME=muffin.CB.V21a_097.BASES-2.config_knowngood
-TESTFILE=xxxx
-CHECKFLUXES=FALSE
--- genie-main/tmp50087.testing.sh
+++
@@ -1,5 +0,0 @@
-TEST_NAME=muffin.CB.V21a_107.BASES-2.config_regtest
-ASSUMEDGOOD_NAME=muffin.CB.V21a_107.BASES-2.config_assumedgood
-KNOWNGOOD_NAME=muffin.CB.V21a_107.BASES-2.config_knowngood
-TESTFILE=xxxx
-CHECKFLUXES=FALSE
