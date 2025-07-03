b658985c5bb67e717088cfd9cf2ec70d8663c8f6
richardstockey <50849895+richardstockey@users.noreply.github.com>
2025-07-03 19:29:09 +0100

    Update cgenie.muffin.scratch.clones.parallel.v2.sh

--- genie-main/cgenie.muffin.scratch.clones.parallel.v2.sh
+++ genie-main/cgenie.muffin.scratch.clones.parallel.v2.sh
@@ -49,6 +49,7 @@ cp -R /scratch/$USER/cgenie.muffin /scratch/$USER/cgenie.muffin-$clone
 cd /scratch/$USER/cgenie.muffin-$clone
 grep -l -r 'cgenie.muffin' --exclude-dir='.git' | xargs sed -i 's/cgenie.muffin/cgenie.muffin-$clone/g'
 cd /scratch/$USER/cgenie.muffin-$clone
+wget -O netcdf.libraries.install.clones.sh https://raw.githubusercontent.com/richardstockey/cgenie.muffin.mix/refs/heads/main/netcdf.libraries.install.clones.sh
 ./netcdf.libraries.install.clones.sh $clone
 # return home
 cd /home/$USER/
